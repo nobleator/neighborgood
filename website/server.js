edc1e0f3531dc51dcefeb663bc25141f187aa2da
nobleator <noble.ben.16@gmail.com>
2018-02-24 17:16:58 -0500

    Config order of params

--- website/server.js
+++ website/server.js
@@ -11,7 +11,7 @@ app.use(express.static( path.join(__dirname, '/public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const config = require('./config') || process.env.DATABASE_URI
+const config = process.env.DATABASE_URI || require('./config')
 console.log(config)
 const pool = new pg.Pool(config)
 
