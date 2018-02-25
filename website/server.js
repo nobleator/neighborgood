875c3f6ff66c6c5935781a81f7935d09a4b0f6c4
nobleator <noble.ben.16@gmail.com>
2018-02-25 18:07:14 -0500

    Updates
    Added node_modules to .gitignore.
    Initialize matrix logic.

--- website/server.js
+++ website/server.js
@@ -6,15 +6,16 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const url = require('url')
 const pg = require('pg')
+const math = require('mathjs')
 const app = express()
 const port = process.env.PORT || 7777
-const env = process.env.NODE_ENV || 'development'
+//const env = process.env.NODE_ENV || 'development'
 
 app.use(express.static( path.join(__dirname, '/public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-var forceSSL = (req, res, next) => {
+/*var forceSSL = (req, res, next) => {
     console.log('in forceSSL, x-forwarded-proto: ', req.headers['x-forwarded-proto'])
     if (req.headers['x-forwarded-proto'] !== 'https') {
         return res.redirect(['https://', req.get('Host'), req.url].join(''))
@@ -24,7 +25,7 @@ var forceSSL = (req, res, next) => {
 console.log('env: ', env)
 if (env === 'production') {
     app.use(forceSSL)
-}
+}*/
 
 const params = url.parse(process.env.DATABASE_URL)
 const auth = params.auth.split(':')
@@ -35,7 +36,7 @@ const config = {
     port: params.port,
     database: params.pathname.split('/')[1]
 }
-console.log(config)
+console.log('Config: ', config)
 const pool = new pg.Pool(config)
 
 app.get('/', (req, res) => {
@@ -72,6 +73,39 @@ app.get('/query', (req, res) => {
         })
     })
 })
+
+// TODO: Feed data into the following code
+var A = math.matrix([[1, 3], [5, 1]])
+console.log(A._data)
+function normalize(mat) {
+    function getColSum(m, cIndx) {
+        var col = math.flatten(math.subset(m, math.index(math.range(0, numberOfRows), colIndx))._data)
+        return col.reduce((a, b) => a + b, 0)
+    }
+    var norm = mat.clone()
+    var size = norm.size()
+    var numberOfRows = size[0]
+    var numberOfCols = size[1]
+    for (var rowIndx = 0; rowIndx < numberOfRows; rowIndx++) {
+        for (var colIndx = 0; colIndx < numberOfCols; colIndx++) {
+            var cellVal = norm.get([rowIndx, colIndx])
+            // norm is changing, so use original mat for colSum
+            var colSum = getColSum(mat, colIndx)
+            var newVal = cellVal / colSum
+            norm.subset(math.index(rowIndx, colIndx), newVal)
+        }
+    }
+    return norm
+}
+var nA = normalize(A)
+var size = nA.size()
+var numberOfRows = size[0]
+for (var rowIndx = 0; rowIndx < numberOfRows; rowIndx++) {
+    var row = nA._data[rowIndx]
+    var rowSum = row.reduce((a, b) => a + b)
+    var rowWeight = rowSum / numberOfRows
+    console.log(row, rowSum, rowWeight)
+}
 
 app.post('/submit', (req, res) => {
     // TODO: Save preference selections
